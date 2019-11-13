193374f249c043a49c8aa581178125b5cdcb2241
Ivaylo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-11-13 15:07:25 +0200

    Add files via upload

---
+++ DefiningClasses/SpeedRacing07/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        private string  model;
+        private double fuelAmmount;
+        private double consPerKilometar;
+        private  double distance;
+
+        public Car(string model, double fuelAmmount, double consPerKilometar)
+        {
+            Model = model;
+            FuelAmmount = fuelAmmount;
+            ConsPerKilometar = consPerKilometar;
+            Distance = 0;
+
+        }
+
+        public  double Distance 
+        {
+            get { return distance; }
+            set { distance = value; }
+        }
+
+        public double ConsPerKilometar
+        {
+            get { return consPerKilometar; }
+            set { consPerKilometar = value; }
+        }
+
+        public double FuelAmmount
+        {
+            get { return fuelAmmount; }
+            set { fuelAmmount = value; }
+        }
+
+        public string  Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public bool Drive(double distance)
+        {
+            if (distance * ConsPerKilometar <= FuelAmmount)
+            {
+                FuelAmmount -= ConsPerKilometar * distance;
+                Distance += distance;
+                return true;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{Model} {FuelAmmount:F2} {Distance}";
+        }
+
+    }
+}
---
+++ DefiningClasses/SpeedRacing07/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+
+                cars.Add(new Car(tokens[0], double.Parse(tokens[1]), double.Parse(tokens[2])));                
+            }
+
+            List<string> data = Console.ReadLine().Split(' ').ToList();
+       
+            while (data[0] != "End")
+            {
+                string model = data[1];
+                double distance = double.Parse(data[2]);
+
+                Car car = cars.First(c => c.Model == model);
+
+                if (!car.Drive(distance))
+                {
+                    Console.WriteLine("Insufficient fuel for the drive");
+                }
+
+                data = Console.ReadLine().Split(' ').ToList();
+            }
+
+            cars.ForEach(Console.WriteLine);
+
+        }
+    }
+}
