aee9ba1334ea307ae7840cd7a150db06e2035ef9
Ivaylo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-11-16 10:35:09 +0200

    Add files via upload

---
+++ DefiningClasses/CarSalesman10/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        private string model;
+        private Engine engine;
+        private int? weight;
+        private string color;
+
+        public Car(string model, Engine engine)
+        {
+            Model = model;
+            Engine = engine;
+        }
+
+        public Car(string model, Engine engine, int? weight)
+            : this(model, engine)
+        {
+            Weight = weight;
+        }
+
+        public Car(string model, Engine engine, string color)
+           : this(model, engine)
+        {
+            Color = color;
+        }
+
+        public Car(string model, Engine engine, int? weight, string color)
+           : this(model, engine)
+        {
+            Weight = weight;
+            Color = color;
+        }
+
+        public int? Weight { get; set; }
+
+        public Engine Engine
+        {
+            get { return engine; }
+            set { engine = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public string Color { get; set; }
+
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine($"{Model}:");
+            builder.Append(Engine);
+            builder.AppendLine($"  Weight: {(Weight == null ? "n/a" : Weight.ToString())}");
+            builder.Append($"  Color: {(Color == null ? "n/a" : Color)}");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ DefiningClasses/CarSalesman10/StartUp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Engine> engines = new List<Engine>();
+            List<Car> cars = new List<Car>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var data = Console.ReadLine().Split();
+
+                string model = data[0];
+                int power = int.Parse(data[1]);
+                                
+                Engine engine = new Engine(model, power);
+
+                if (data.Length == 3)
+                {
+                    if (int.TryParse(data[2], out int result))
+                    {
+                        engine.Displacement = result;
+                    }
+                    else
+                    {
+                        engine.Efficiency = data[2];
+                    }
+                }
+                else if (data.Length == 4)
+                {
+                    engine.Displacement = int.Parse(data[2]);
+                    engine.Efficiency = data[3];
+                }
+            
+                engines.Add(engine);
+            }
+
+            
+            int m = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                var data = Console.ReadLine().Split();
+
+                string model = data[0];
+                Engine engine = engines.First(e=>e.Model == data[1]);
+
+                Car car = new Car(model, engine);
+
+                if (data.Length == 3)
+                {
+                    if (int.TryParse(data[2], out int result))
+                    {
+                        car.Weight = result;
+                    }
+                    else
+                    {
+                        car.Color = data[2];
+                    }
+                }
+                else if (data.Length == 4)
+                {
+                    car.Weight = int.Parse(data[2]);
+                    car.Color = data[3];
+                }
+
+                cars.Add(car);
+            }
+
+            cars.ForEach(Console.WriteLine);
+        }
+    }
+}
